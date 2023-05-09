b0fa683ee850535023b465c3d5ddb7a2f97ebba2
Naa003 <nathan.anderson.1024@gmail.com>
2023-05-09 12:59:26 -0700

    intial navbar creation

--- view/src/App.js
+++ view/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import NavBar from './components/NavBar';
+
 
 function App() {
   return (
-    <div className="App">
+    <div className='App '>
+      <NavBar></NavBar>
     </div>
   );
 }
