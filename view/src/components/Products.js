418bf65a7e4cee7dda56fb724d4d3412de7d8573
Naa003 <nathan.anderson.1024@gmail.com>
2023-05-17 17:35:20 -0700

    changed links

--- view/src/components/Products.js
+++ view/src/components/Products.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export default function Products({products}) {
   return (
     <div className='grid lg:grid-cols-4 gap-6 m-2 md:grid-cols-3 sm-grid-cols-2 justify-items-center content-center'>
@@ -7,12 +7,14 @@ export default function Products({products}) {
         return (
           <div key={index} className='border-2 border-gray-200 pb-1'>
             <div>
-              <a href={`/products/${index}`}>
+              <Link to={`/products/${index}`}>
                 <h2 className='font-bold'>{product.name}</h2>
                 <p className='text-green-700 font-bold'>${product.price_array[product.price_array.length - 1]}</p>
-              </a>
+              </Link>
             </div>
-            <button className='mt-1 border rounded-full px-4 py-1 text-sm text-blue-700 font-semibold border-blue-700 hover:text-white hover:bg-blue-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2'><a href={`/products/${index}`}>More Info</a></button>
+            <button className='mt-1 border rounded-full px-4 py-1 text-sm text-blue-700 font-semibold border-blue-700 hover:text-white hover:bg-blue-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2'>
+              <Link to={`/products/${index}`}>More Info</Link>
+            </button>
           </div>
         )
       })}
