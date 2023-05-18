f3b6e4cc251f6b4fe8a152fe6e61441d8eddd948
Naa003 <nathan.anderson.1024@gmail.com>
2023-05-17 17:33:18 -0700

    started filling in info for product

--- view/src/components/Product.js
+++ view/src/components/Product.js
@@ -7,7 +7,12 @@ export default function Product({products}) {
 
   return (
     <div>
-      {findProduct.name}
+      <div className="text-center">
+        <h1>{findProduct.name}</h1>
+        <h2>Current Price: ${findProduct.price_array[findProduct.price_array.length - 1]}</h2>
+        <h3>Last Updated: {findProduct.added_array[findProduct.added_array.length - 1]}</h3>
+      </div>
+      <div></div>
     </div>
   )
 }
