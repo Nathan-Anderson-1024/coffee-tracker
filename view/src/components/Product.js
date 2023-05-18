0cf076f2769de89f121194b541f3074fef93f264
Naa003 <nathan.anderson.1024@gmail.com>
2023-05-18 14:35:34 -0700

    styling product table

--- view/src/components/Product.js
+++ view/src/components/Product.js
@@ -6,13 +6,46 @@ export default function Product({products}) {
   const findProduct = products.find((product, index) => index === parseInt(id));
 
   return (
-    <div>
+    <div className='mt-12'>
       <div className="text-center">
         <h1>{findProduct.name}</h1>
         <h2>Current Price: ${findProduct.price_array[findProduct.price_array.length - 1].toFixed(2)}</h2>
-        <h3>Last Updated: {findProduct.added_array[findProduct.added_array.length - 1]}</h3>
+        <h3>Last Updated: {findProduct.added_array[findProduct.added_array.length - 1]} from Amazon.</h3>
       </div>
-      <div></div>
+      <div className='flex justify-center mt-12'>
+        <table className='table-auto w-3/6 border-solid border-2 shadow-lg'>
+          <thead className=''>
+            <tr>
+              <th className='bg-gray-100 border py-4 text-center'>Crawl Date</th>
+              <th className='bg-gray-100 border text-center py-4'>Price</th>
+            </tr>
+          </thead>
+          
+          <tbody className='text-center'>
+          {findProduct.added_array.map((date, index) => {
+              return (
+                <tr key={index}>
+                  <td className='border py-2'>{new Date(date).toISOString().split('T')[0]}</td>
+                  <td className='border py-2'>{findProduct.price_array[index]}</td>
+                </tr>
+                
+              )
+              
+            })}
+            {/* {findProduct.price_array.map((price) => {
+              return (
+                <tr>
+                  <td>{price}</td>
+                </tr>
+                
+              )
+            })} */}
+          
+          
+            
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
--- view/src/components/Products.js
+++ view/src/components/Products.js
@@ -5,14 +5,14 @@ export default function Products({products}) {
     <div className='grid lg:grid-cols-4 gap-6 m-2 md:grid-cols-3 sm-grid-cols-2 justify-items-center content-center'>
       {products.map((product, index) => {
         return (
-          <div key={index} className='border-2 border-gray-200 pb-1'>
+          <div key={index} className='border-2 border-gray-200 pb-1 '>
             <div>
               <Link to={`/products/${index}`}>
                 <h2 className='font-bold'>{product.name}</h2>
                 <p className='text-green-700 font-bold'>${product.price_array[product.price_array.length - 1].toFixed(2)}</p>
               </Link>
             </div>
-            <button className='mt-1 border rounded-full px-4 py-1 text-sm text-blue-700 font-semibold border-blue-700 hover:text-white hover:bg-blue-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2'>
+            <button className='mt-1 border rounded-full px-4 py-1 text-sm text-blue-700 font-semibold border-gray-900 hover:text-white hover:bg-gray-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2'>
               <Link to={`/products/${index}`}>More Info</Link>
             </button>
           </div>
