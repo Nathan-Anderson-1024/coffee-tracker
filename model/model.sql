b5464d0028fd6c9f558cf4bd6cec9e38bd363f69
Naa003 <nathan.anderson.1024@gmail.com>
2023-05-19 15:44:13 -0700

    updated db config and centered imgs

--- model/model.sql
+++ model/model.sql
@@ -5,5 +5,6 @@ CREATE TABLE coffee (
   name varchar(255) NOT NULL, 
   price numeric(8,4) NOT NULL,
   date_added date NOT NULL,
-  vendor varchar(255) NOT NULL
+  vendor varchar(255) NOT NULL,
+  img varchar(255)
 );
