92412dccca77d3e99d6c321b2e4efae0af261973
Naa003 <nathan.anderson.1024@gmail.com>
2023-05-30 17:09:25 -0700

    started layout of settings page

--- view/src/pages/Settings.js
+++ view/src/pages/Settings.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Settings() {
   return (
-    <div>Settings</div>
+    <div className='flex flex-col justify-center items-center'>
+      {/* TODO:
+        -[] Add inputs to allow users to change user info and update the server
+      
+      */}
+      <h1>First Name</h1>
+      <h1>Last Name</h1>
+      <h1>Password</h1>
+      <h1>Email</h1>
+      <h1>Username</h1>
+    </div>
   )
 }
