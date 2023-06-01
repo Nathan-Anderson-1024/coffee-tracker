861c1b81676bbf49f2fa9d863cdc0e0881c60bcc
Naa003 <nathan.anderson.1024@gmail.com>
2023-05-31 17:25:11 -0700

    added styling to settings page and default info

--- view/src/App.js
+++ view/src/App.js
@@ -54,7 +54,7 @@ function App() {
         <Route path="/caffeine" element={<Caffeine />}></Route>
         <Route path="/analyze" element={<TrendAnalysis />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
-        <Route path="/settings" element={<Settings />}></Route>
+        <Route path="/settings" element={<Settings username={username} fullName={fullName} />}></Route>
         <Route path='/products/:id' element={<Product products={productList} />}></Route>
         <Route path="/login" element={<Login loading={loading} setLoading={setLoading} login={login} setLogin={setLogin} username={username} setUsername={setUsername} setError={setError} error={error} fullName={fullName} setFullName={setFullName} />}></Route>
         <Route path='/register' element={<Register loading={loading} setLoading={setLoading} />}></Route>
--- view/src/pages/Settings.js
+++ view/src/pages/Settings.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Settings() {
+export default function Settings({username, fullName}) {
   return (
     <form className='flex flex-col justify-center items-center mt-5'>
       {/* TODO:
@@ -9,20 +9,20 @@ export default function Settings() {
       */}
       
       <label htmlFor='firstName'>First Name</label>
-      <input id='firstName' className='border rounded border-slate-900'></input>
+      <input id='firstName' className='border rounded border-slate-900' value={fullName.split(" ")[0]}></input>
       
       <label htmlFor='lastName'>Last Name</label>
-      <input id='lastName' className='border rounded border-slate-900'></input>
+      <input id='lastName' className='border rounded border-slate-900' value={fullName.split(" ")[1]}></input>
       
       <label htmlFor='userName'>Username</label>
-      <input id='userName' className='border rounded border-slate-900'></input>
+      <input id='userName' className='border rounded border-slate-900 text-black' value={username}></input>
       
       <label htmlFor='password'>Password</label>
       <input id='password' className='border rounded border-slate-900'></input>
       
       <label htmlFor='email'>Email</label>
       <input id='email' className='border rounded border-slate-900'></input>
-      
+      <button className='border border-slate-900 rounded mt-5 p-2 bg-blue-700 text-white'>Update Information</button>
     </form>
   )
 }
